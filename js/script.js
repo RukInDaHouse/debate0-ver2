cc424a1f43577557e82ad62ccd456d9226b96c41
Peter Rukin <61048011+RukInDaHouse@users.noreply.github.com>
2021-02-20 16:37:34 +0700

    Create new version of script without canvas

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let distance = 550;
+
+function change_color(elem, color) {
+    elem.parentElement.style.background = color;
+}
+
+let count = 0;
+
+document.body.addEventListener("click", function (e) {
+    if (e.target.classList.contains("btn")) {
+        let div = document.createElement("div"),
+            father = e.path[5];
+            count = count + 10;
+        div.classList = father.classList;
+        div.style.left = father.offsetLeft + distance + "px";
+        div.style.top = father.offsetTop + "%";
+
+        let arrow_width = distance - father.offsetWidth;
+        div.innerHTML =
+            father.innerHTML +
+            `<div class='arrow' style='left: ${-arrow_width}px;width:${arrow_width}px;'><div></div></div>`;
+        document.body.append(div);
+    }
+});
