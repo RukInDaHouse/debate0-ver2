98f32577e7e4a0aeba89ea8ed3a79f1b7d642a67
Peter Rukin <61048011+RukInDaHouse@users.noreply.github.com>
2021-02-20 23:51:24 +0700

    Test layout with colums

--- js/script.js
+++ js/script.js
@@ -10,10 +10,10 @@ document.body.addEventListener("click", function (e) {
     if (e.target.classList.contains("btn")) {
         let div = document.createElement("div"),
             father = e.path[5];
-            count = count + 10;
+            count = count + 100;
         div.classList = father.classList;
         div.style.left = father.offsetLeft + distance + "px";
-        div.style.top = father.offsetTop + "%";
+        div.style.top = father.offsetTop + count + "px";
 
         let arrow_width = distance - father.offsetWidth;
         div.innerHTML =
