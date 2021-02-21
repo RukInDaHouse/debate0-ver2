7e386d10721e511ecd33c17435bbd95035842b8b
Peter Rukin <61048011+RukInDaHouse@users.noreply.github.com>
2021-02-21 21:58:54 +0700

    Begin of DOM changing

--- js/script.js
+++ js/script.js
@@ -1,24 +1,37 @@
-let distance = 550;
+let distance = 250;
 
 function change_color(elem, color) {
     elem.parentElement.style.background = color;
 }
 
-let count = 0;
-
 document.body.addEventListener("click", function (e) {
     if (e.target.classList.contains("btn")) {
         let div = document.createElement("div"),
             father = e.path[5];
-            count = count + 100;
         div.classList = father.classList;
         div.style.left = father.offsetLeft + distance + "px";
-        div.style.top = father.offsetTop + count + "px";
+        div.style.top = father.offsetTop + "px";
 
         let arrow_width = distance - father.offsetWidth;
         div.innerHTML =
             father.innerHTML +
-            `<div class='arrow' style='left: ${-arrow_width}px;width:${arrow_width}px;'><div></div></div>`;
+            `<div class='arrow' style='left: ${-arrow_width}px;width:${arrow_width}px;'></div>`;
         document.body.append(div);
+    }
+});
+
+document.body.addEventListener("click", function (e) {
+    if (e.target.classList.contains("btn2")) {
+        var div = document.createElement("div");
+        var father = e.path[3];
+            col = document.getElementsByClassName('col')[4];
+            col.appendChild(div);
+        div.classList = father.classList;
+        div.classList.remove("col");
+
+        let arrow_width = distance - father.offsetWidth;
+        div.innerHTML =
+            father.innerHTML +
+            `<div class='arrow' style='right: ${-arrow_width}px;width:${arrow_width}px;'></div>`;
     }
 });
