0b8163832dfaa99f9bd3184ccc26afa30adb9356
Peter Rukin <61048011+RukInDaHouse@users.noreply.github.com>
2021-02-21 23:51:15 +0700

    Add cards creations

--- js/script.js
+++ js/script.js
@@ -21,17 +21,43 @@ document.body.addEventListener("click", function (e) {
 });
 
 document.body.addEventListener("click", function (e) {
-    if (e.target.classList.contains("btn2")) {
+    if (e.target.classList.contains("lvl1")) {
         var div = document.createElement("div");
-        var father = e.path[3];
-            col = document.getElementsByClassName('col')[4];
+        var father = e.path[5];
+            col = document.getElementsByClassName('col')[8];
             col.appendChild(div);
         div.classList = father.classList;
-        div.classList.remove("col");
+
 
         let arrow_width = distance - father.offsetWidth;
         div.innerHTML =
             father.innerHTML +
             `<div class='arrow' style='right: ${-arrow_width}px;width:${arrow_width}px;'></div>`;
+        
+            var lvl = div.getElementsByClassName('lvl1')[0];
+            lvl.classList.add("lvl2");
+            lvl.classList.remove("lvl1");
+            div.classList.remove("col");
+    }
+});
+
+document.body.addEventListener("click", function (e) {
+    if (e.target.classList.contains("lvl2")) {
+        var div = document.createElement("div");
+        var father = e.path[5];
+            col = document.getElementsByClassName('col')[6];
+            col.appendChild(div);
+        div.classList = father.classList;
+
+
+        let arrow_width = distance - father.offsetWidth;
+        div.innerHTML =
+            father.innerHTML +
+            `<div class='arrow' style='right: ${-arrow_width}px;width:${arrow_width}px;'></div>`;
+        
+            var lvl = div.getElementsByClassName('lvl2')[0];
+            lvl.classList.add("lvl3");
+            lvl.classList.remove("lvl2");
+            div.classList.remove("col");
     }
 });
