bbb62444157f17ebfc1c934d0579c20d5b7df72b
Peter Rukin <61048011+RukInDaHouse@users.noreply.github.com>
2021-02-25 23:00:23 +0700

    Add zoomin/zoomout function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
-let distance = 250;
-
 function change_color(elem, color) {
     elem.parentElement.style.background = color;
 }
+
+document.body.addEventListener("click", function (e) {
+    if (e.target.classList.contains("zoomin")) {  
+		var father = this;
+		var card = father.getElementsByClassName("card")[0];
+			card.style.height = card.offsetHeight + 50 + 'px';
+			card.style.width = card.offsetWidth + 50 + 'px';
+    }
+});
+
+document.body.addEventListener("click", function (e) {
+    if (e.target.classList.contains("zoomout")) {  
+		var father = this;
+		var card = father.getElementsByClassName("card")[0];
+			card.style.height = card.offsetHeight - 50 + 'px';
+			card.style.width = card.offsetWidth - 50 + 'px';
+    }
+});
 
 document.body.addEventListener("click", function (e) {
     if (e.target.classList.contains("lvl1blue")) {
@@ -12,11 +28,7 @@ document.body.addEventListener("click", function (e) {
 		var content = col.getElementsByClassName('content')[0];
 			content.appendChild(div);
         div.classList = father.classList;
-
-        let arrow_width = distance - father.offsetWidth;
-        div.innerHTML =
-            father.innerHTML +
-            `<div class='arrow' style='right: ${-arrow_width}px;width:${arrow_width}px;'><div></div></div>`;
+        div.innerHTML = father.innerHTML;
         
         var btn_upper = div.getElementsByClassName('btn2')[0];
 			btn_upper.classList.add("lvl2purple");
@@ -37,14 +49,8 @@ document.body.addEventListener("click", function (e) {
         var col = document.getElementsByClassName('col')[12];
 		var content = col.getElementsByClassName('content')[0];
 			content.appendChild(div);
-        	div.classList = father.classList;
-
-		var color = div.getElementsByClassName("card")[0];
-			
-        let arrow_width = distance - father.offsetWidth;
-        div.innerHTML =
-            father.innerHTML +
-            `<div class='arrow' style='right: ${-arrow_width}px;width:${arrow_width}px;'><div></div></div>`;
+        div.classList = father.classList;			
+        div.innerHTML = father.innerHTML;
         
 		var btn_upper = div.getElementsByClassName('btn2')[0];
 			btn_upper.classList.add("lvl2purple");
@@ -58,6 +64,28 @@ document.body.addEventListener("click", function (e) {
 		var card = div.getElementsByClassName('card')[0];
 			card.classList.add("purple");
 			card.classList.remove("blue");
+    }
+});
+
+document.body.addEventListener("click", function (e) {
+    if (e.target.classList.contains("lvl2blue")) {
+        var div = document.createElement("div");
+        var father = e.path[5];
+        var col = document.getElementsByClassName('col')[6];
+		var content = col.getElementsByClassName('content')[0];
+			content.appendChild(div);
+        div.classList = father.classList;
+        div.innerHTML = father.innerHTML;
+        
+        var btn_upper = div.getElementsByClassName('btn2')[0];
+			btn_upper.classList.add("lvl2purple");
+			btn_upper.classList.remove("lvl1purple");
+		var btn_lower = div.getElementsByClassName('btn2')[1];
+			btn_lower.classList.add("lvl2blue");
+			btn_lower.classList.remove("lvl1blue");
+		
+		div.classList.remove("col");
+
     }
 });
 
