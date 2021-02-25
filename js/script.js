d1711b8a6f55669cc5d1dc3393bc7f00911fad8e
Peter Rukin <61048011+RukInDaHouse@users.noreply.github.com>
2021-02-25 18:08:29 +0700

    Add purple blocks functions

--- js/script.js
+++ js/script.js
@@ -5,32 +5,33 @@ function change_color(elem, color) {
 }
 
 document.body.addEventListener("click", function (e) {
-    if (e.target.classList.contains("lvl1")) {
+    if (e.target.classList.contains("lvl1blue")) {
         var div = document.createElement("div");
         var father = e.path[5];
         var col = document.getElementsByClassName('col')[8];
 		var content = col.getElementsByClassName('content')[0];
 			content.appendChild(div);
-        	div.classList = father.classList;
-
+        div.classList = father.classList;
 
         let arrow_width = distance - father.offsetWidth;
         div.innerHTML =
             father.innerHTML +
             `<div class='arrow' style='right: ${-arrow_width}px;width:${arrow_width}px;'><div></div></div>`;
         
-        var lvl = div.getElementsByClassName('lvl1')[0];
-			lvl.classList.add("lvl2right");
-			lvl.classList.add("lvl2");
-			lvl.classList.remove("lvl1right");
-			lvl.classList.remove("lvl2");
-			div.classList.remove("col");
+        var btn_upper = div.getElementsByClassName('btn2')[0];
+			btn_upper.classList.add("lvl2purple");
+			btn_upper.classList.remove("lvl1purple");
+		var btn_lower = div.getElementsByClassName('btn2')[1];
+			btn_lower.classList.add("lvl2blue");
+			btn_lower.classList.remove("lvl1blue");
+		
+		div.classList.remove("col");
 
     }
 });
 
 document.body.addEventListener("click", function (e) {
-    if (e.target.classList.contains("lvl1right")) {
+    if (e.target.classList.contains("lvl1purple")) {
         var div = document.createElement("div");
         var father = e.path[5];
         var col = document.getElementsByClassName('col')[12];
@@ -38,20 +39,25 @@ document.body.addEventListener("click", function (e) {
 			content.appendChild(div);
         	div.classList = father.classList;
 
+		var color = div.getElementsByClassName("card")[0];
+			
         let arrow_width = distance - father.offsetWidth;
         div.innerHTML =
             father.innerHTML +
             `<div class='arrow' style='right: ${-arrow_width}px;width:${arrow_width}px;'><div></div></div>`;
         
-        var lvl = div.getElementsByClassName('lvl1')[0];
-            lvl.classList.add("lvl2right");
-			lvl.classList.add("lvl2");
-            lvl.classList.remove("lvl1right");
-			lvl.classList.remove("lvl2");
-            div.classList.remove("col");
-			
+		var btn_upper = div.getElementsByClassName('btn2')[0];
+			btn_upper.classList.add("lvl2purple");
+			btn_upper.classList.remove("lvl1purple");
+		var btn_lower = div.getElementsByClassName('btn2')[1];
+			btn_lower.classList.add("lvl2blue");
+			btn_lower.classList.remove("lvl1blue");
+		
+		div.classList.remove("col");
+
 		var card = div.getElementsByClassName('card')[0];
-            card.classList.add("purple");
-            card.classList.remove("blue");
+			card.classList.add("purple");
+			card.classList.remove("blue");
     }
 });
+
